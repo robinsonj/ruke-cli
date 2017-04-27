d9122ffa5ce86c714286807be11c61502effde35
Joseph L. Robinson <robinsonjosephlewis@gmail.com>
2017-04-26 17:00:35 -0700

    Start main event loop

--- src/duke/mod.rs
+++ src/duke/mod.rs
@@ -1,13 +1,39 @@
-pub struct Game {
+#[derive(Debug)]
+pub enum GameState {
+  GameStart,
+  GameRun
+}
 
+pub struct Game {
+  state: GameState
 }
 
 impl Game {
   pub fn new() -> Game {
-    Game {}
+    Game {
+      state: GameState::GameStart
+    }
   }
 
   pub fn run(&mut self) {
     println!("Hello world!");
+
+    loop {
+      match self.state {
+        GameState::GameStart => {
+          self.setup();
+          self.state = GameState::GameRun;
+        },
+        GameState::GameRun => {
+          println!("Running game!");
+          break;
+        },
+        // _ => ()
+      }
+    }
+  }
+
+  pub fn setup(&mut self) {
+
   }
 }
